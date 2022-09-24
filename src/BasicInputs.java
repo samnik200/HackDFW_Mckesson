f18f7d4dbb2c860e1348f04c9ede0b6a617ecf22
Caleb LaRue <cr.larue@ou.edu>
2022-09-24 10:53:19 -0500

    Add the constructors

--- src/BasicInputs.java
+++ src/BasicInputs.java
@@ -3,10 +3,34 @@ public class BasicInputs
 	private int height;
 	private int weight;
 	private boolean recurring;
-	private String KnownConditions;
+	private String knownConditions;
 	private String explainSymptoms;
-	public BasicInputs()
+	public BasicInputs(int height, int weight, boolean recurring, String knownConditions, String explainSymptoms)
 	{
-		
+		this.height = height;
+		this.weight = weight;
+		this.recurring = recurring;
+		this.knownConditions = knownConditions;
+		this.explainSymptoms = explainSymptoms;
 	}
+	public BasicInputs(int height, int weight, boolean recurring, String knownConditions)
+	{
+		this.height = height;
+		this.weight = weight;
+		this.recurring = recurring;
+		this.knownConditions = knownConditions;
+	}
+	public BasicInputs(int height, int weight, boolean recurring)
+	{
+		this.height = height;
+		this.weight = weight;
+		this.recurring = recurring;
+	}
+	public BasicInputs(int height, int weight)
+	{
+		this.height = height;
+		this.weight = weight;
+	}
+	
+	
 }
