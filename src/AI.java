0518a9d36b66cff1ba3593a5725d41c3f117034e
samnik200 <91707963+samnik200@users.noreply.github.com>
2022-09-24 11:19:54 -0500

    ai update

--- src/AI.java
+++ src/AI.java
@@ -6,7 +6,11 @@ public class AI {
     private String[] Symptom = input_patient.split("");
     void Symptom_Calc(String[] str)
     {
+        for (int j =0;j<Symptom.length;j++)
+            for (int i=0;i<Symptom.length;i++)
+            {
+                int test = PossibleSymptoms[j].compareToIgnoreCase(Symptom[i]);
+            }
 
-     
     }
 }
