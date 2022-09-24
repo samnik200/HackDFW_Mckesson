0304ca36d9979f718673568510c171c5a98f2350
samnik200 <91707963+samnik200@users.noreply.github.com>
2022-09-24 10:43:56 -0500

    ok

--- src/AI.java
+++ src/AI.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
+import java.util.regex.Pattern;
 public class AI {
     String PossibleSymptoms[]={"headache",""};
     void Symptom_Calc(String[] str)
     {
-        
+     if()
+     {
+
+     }
     }
 }
