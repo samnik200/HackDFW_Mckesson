ab417a78875088592ba17d198f548f597d843097
samnik200 <91707963+samnik200@users.noreply.github.com>
2022-09-24 12:03:17 -0500

    added logic for algo
    added words and made the basic logic for the algorithm. now have to fully finish the algo

--- src/AI.java
+++ src/AI.java
@@ -2,15 +2,25 @@ import java.util.Arrays;
 import java.util.regex.Pattern;
 public class AI {
     String input_patient;
-    private String PossibleSymptoms[]={"headache",""};
+    private int test;
+    private int i;
+    private int j;
+    private int [] indx;
+    private int secondary=0;
+    private String PossibleSymptoms[]={"headache","pain", "urination","bumps", "genitals" , "coughing", "soar", "throat", "rashes", "diarrhea", "vomiting", "runny", "nose"};
     private String[] Symptom = input_patient.split("");
     void Symptom_Calc(String[] str)
     {
-        for (int j =0;j<Symptom.length;j++)
-            for (int i=0;i<Symptom.length;i++)
+        for (j =0;j<Symptom.length;j++)
+            if ( test ==0)
             {
-                int test = PossibleSymptoms[j].compareToIgnoreCase(Symptom[i]);
+                indx[secondary]=j;
             }
+            for (i=0;i<Symptom.length;i++)
+            {
+                 test = PossibleSymptoms[j].compareToIgnoreCase(Symptom[i]);
+            }
+
 
     }
 }
