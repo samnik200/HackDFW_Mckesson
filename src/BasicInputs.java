7c2a997625e25a9d29adc86cbee9efd86e06cb8a
Caleb LaRue <cr.larue@ou.edu>
2022-09-24 10:57:35 -0500

    Add getters and setters + comment on Params

--- src/BasicInputs.java
+++ src/BasicInputs.java
@@ -5,6 +5,19 @@ public class BasicInputs
 	private boolean recurring;
 	private String knownConditions;
 	private String explainSymptoms;
+	/**
+	 * 
+	 * @param height
+	 * the Height of the patient
+	 * @param weight
+	 * the Weight of the patient
+	 * @param recurring
+	 * if a symptom is recurring
+	 * @param knownConditions
+	 * Patient explaining known Conditions
+	 * @param explainSymptoms
+	 * Patient explaining symptoms
+	 */
 	public BasicInputs(int height, int weight, boolean recurring, String knownConditions, String explainSymptoms)
 	{
 		this.height = height;
@@ -30,6 +43,36 @@ public class BasicInputs
 	{
 		this.height = height;
 		this.weight = weight;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public boolean isRecurring() {
+		return recurring;
+	}
+	public void setRecurring(boolean recurring) {
+		this.recurring = recurring;
+	}
+	public String getKnownConditions() {
+		return knownConditions;
+	}
+	public void setKnownConditions(String knownConditions) {
+		this.knownConditions = knownConditions;
+	}
+	public String getExplainSymptoms() {
+		return explainSymptoms;
+	}
+	public void setExplainSymptoms(String explainSymptoms) {
+		this.explainSymptoms = explainSymptoms;
 	}
 	
 	
